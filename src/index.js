790d602e6509a467e89d7efb41d38cb026ab6c8a
ffybcesin <ffybcesin@gmail.com>
2025-07-02 08:45:19 -0300

    subiendo proyecto 2da entrega

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const app.express();
-app.get("/",(req,res) =>{
-    res.send("hola");
-});
-
-const PORT=3000;
-app.listen(PORT,()=>console.log(`http://localhost:${PORT}`));
